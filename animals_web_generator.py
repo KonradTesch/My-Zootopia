1feabb40efa2f7e9874d32245230dba1feed4abd
Konrad Tesch <konrad.tesch@gmx.de>
2025-07-11 18:50:13 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -25,9 +25,39 @@ def display_data(data):
 
         print()
 
+def generate_html_string(data):
+    html_string = ""
+    for animal in data:
+        name = animal['name']
+        html_string += f"Name: {name}\n"
+
+        if 'diet' in animal['characteristics'].keys():
+            diet = animal['characteristics']['diet']
+            html_string += f"Diet: {diet}\n"
+
+        locations = animal['locations']
+        locations = ", ".join(locations)
+        html_string += f"Location: {locations}\n"
+
+        if 'type' in animal['characteristics'].keys():
+            animal_type = animal['characteristics']['type']
+            html_string += f"Type: {animal_type}\n\n"
+
+    return html_string
+
+def manipulate_html_file(html_string):
+    with open('animals_template.html', 'r') as file:
+        content = file.read()
+
+    content = content.replace("__REPLACE_ANIMALS_INFO__", html_string)
+
+    with open('animals_template.html', 'w') as file:
+        file.write(content)
+
 def main():
     data = load_data()
-    display_data(data)
+    html_string = generate_html_string(data)
+    manipulate_html_file(html_string)
 
 
 if __name__ == '__main__':
